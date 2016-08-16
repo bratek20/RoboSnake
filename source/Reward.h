14cf334f22e801a787b7429a8958f0f004bf0afa
Bartosz Rudzki <bartoszrudzkilo14@gmail.com>
2016-08-16 22:01:40 +0200

    Poprawa szansy nagradzania strongTail.

--- source/Reward.h
+++ source/Reward.h
@@ -17,7 +17,7 @@ private:
     int currentTime; //aktualny czas potrzebny do okreslenia widocznosci
     const int spawnTime = 10; // czas w klatkach co ile pojawia sie nowa nagroda
     const int rewardAmount = 3; //ilosc przydzielen nagrody - o ile segmentow wydluzy sie snake przy jednym zebraniu
-    const int strongTailPropability = 3111; //szansa na pojawienie sie jako nagordy strongTail wynosi 1/strongTailPopability
+    const int strongTailPropability = 3; //szansa na pojawienie sie jako nagordy strongTail wynosi 1/strongTailPopability
     TailType rewardType; //rodzaj nagrody
     void reset(); //ustawia nowa nagrode, jej pozycje typ itp
     void randomPosition();//losuje nowa pozycje nagrody tak aby nie kolidowala z przeszkodami na scenie gry
